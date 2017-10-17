2785937e979fd4134079f7704c99708b19f5fc34
Yuting Wang <yuting.wang@yale.edu>
2017-10-17 14:20:19 -0400

    More test cases

--- x86model/Model/decenc_tests/tst_add.ml
+++ x86model/Model/decenc_tests/tst_add.ml
@@ -20,6 +20,12 @@ let encode instr =
   | Some bits -> List.map (byte_dec_to_hex <| Big.to_int) bits
 
 
+(** The following test cases come from the course at
+
+    http://www.c-jump.com/CIS77/CPU/x86/index.html
+
+**)
+
 (* 'AAA' *)
 let aaa = encode AAA
 
@@ -29,6 +35,7 @@ let ebx = Reg_op EBX
 let ecx = Reg_op ECX
 let edx = Reg_op EDX
 let edi = Reg_op EDI
+let ebp = Reg_op EBP
 
 (* 'add cl, al' should be encoded as '02 C8' *)
 let add_cl_al = ADD (false, ecx, eax)
@@ -38,13 +45,27 @@ let () = assert (encode add_cl_al = ["02"; "C8"])
 let add_ecx_eax = ADD (true, ecx, eax)
 let () = assert (encode add_ecx_eax = ["03"; "C8"])
 
-(* 'add edx, 0x0F0E' should be encoded as '03 15 0E 0F 00 00' *)
-let add_edx_0f0e = 
+(* 'add edx, [0x0F0E]' should be encoded as '03 15 0E 0F 00 00' *)
+let add_edx_disp32 = 
   ADD (true, edx, 
        Address_op {addrDisp = Big.of_int 0x0F0E; addrBase = None; addrIndex = None})
-let () = assert (encode add_edx_0f0e = ["03"; "15"; "0E"; "0F"; "00"; "00"])
+let () = assert (encode add_edx_disp32 = ["03"; "15"; "0E"; "0F"; "00"; "00"])
 
-(* 'add edi, [ebx]' should be encoded as *)
+(* 'add edi, [ebx]' should be encoded as '03 3B'*)
 let add_edi_ind_ebx = 
   ADD (true, edi, 
        Address_op {addrDisp = Big.zero; addrBase = Some EBX; addrIndex = None})
+let () = assert (encode add_edi_ind_ebx = ["03"; "3B"])
+
+(* 'add eax, [esi + 0x1D]' should be encoded as '03 46 1D*)
+let add_eax_esi_disp8 = 
+  ADD (true, eax,
+       Address_op {addrDisp = Big.of_int 0x1D; addrBase = Some ESI; addrIndex = None})
+let () = assert (encode add_eax_esi_disp8 = ["03"; "46"; "1D"])
+
+(* 'add ebx, [ebp + 0x1D34]' should be encoded as '03 9D 34 1D 00 00*)
+let add_ebx_ebp_disp32 = 
+  ADD (true, ebx,
+       Address_op {addrDisp = Big.of_int 0x1D34; addrBase = Some EBP; addrIndex = None})
+let () = assert (encode add_ebx_ebp_disp32 = ["03"; "9D"; "34"; "1D"; "00"; "00"])
+
